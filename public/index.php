137d235666f9c46e54e4132b79bddf23172caac3
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-05-12 20:01:37 +0200

    first template using twig

--- public/index.php
+++ public/index.php
@@ -3,12 +3,15 @@
 require_once '../vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => '../compilation_cache',
-]);
+// $twig = new \Twig\Environment($loader, [
+//     'cache' => '../compilation_cache',
+// ]);
+$twig = new \Twig\Environment($loader);
 
 $navigation = array(
     array ('href' => "index.php", 'caption' => 'home'),
+    array ('href' => "index.php", 'caption' => 'blog'),
+    array ('href' => "index.php", 'caption' => 'register'),
 );
 
 $template = $twig->load('home.twig');
