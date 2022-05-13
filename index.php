87ea1e9088062f265692bd5334a23f62292e7f79
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-05-13 17:16:55 +0200

    adaptation of the structure from OC online course (need twig conversion)

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+// require_once '../vendor/autoload.php';
+
+// $loader = new \Twig\Loader\FilesystemLoader('../templates');
+// // $twig = new \Twig\Environment($loader, [
+// //     'cache' => '../compilation_cache',
+// // ]);
+// $twig = new \Twig\Environment($loader);
+
+// $navigation = array(
+//     array ('href' => "index.php", 'caption' => 'home'),
+//     array ('href' => "index.php", 'caption' => 'blog'),
+//     array ('href' => "index.php", 'caption' => 'register'),
+// );
+
+// $template = $twig->load('home.twig');
+// echo $template->render([
+//     'navigation' => $navigation,
+//     'a_variable' => 5
+// ]);
+
+require('src/controller/frontend.php');
+
+try {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                }
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+    }
+    else {
+        listPosts();
+    }
+}
+catch(Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
+}
