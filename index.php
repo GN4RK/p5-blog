fc600f89f6d5f9132da676b5b7168edd67820064
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-05-16 15:20:36 +0200

    clean up

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
 
-// require_once '../vendor/autoload.php';
-
-// $loader = new \Twig\Loader\FilesystemLoader('../templates');
-// // $twig = new \Twig\Environment($loader, [
-// //     'cache' => '../compilation_cache',
-// // ]);
-// $twig = new \Twig\Environment($loader);
-
-// $navigation = array(
-//     array ('href' => "index.php", 'caption' => 'home'),
-//     array ('href' => "index.php", 'caption' => 'blog'),
-//     array ('href' => "index.php", 'caption' => 'register'),
-// );
-
-// $template = $twig->load('home.twig');
-// echo $template->render([
-//     'navigation' => $navigation,
-//     'a_variable' => 5
-// ]);
-
 require('src/controller/frontend.php');
 require_once 'vendor/autoload.php'; // Twig
 
