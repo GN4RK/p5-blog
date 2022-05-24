93276bb8f4c185cc5732321ddca0470a1d4b424b
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-05-24 17:01:45 +0200

    added header of a blog post

--- src/model/PostManager.php
+++ src/model/PostManager.php
@@ -7,7 +7,7 @@ class PostManager extends Manager
     {
         $db = $this->dbConnect();
         $req = $db->query(
-            'SELECT id, title, content, DATE_FORMAT(last_update, \'%d/%m/%Y à %Hh%imin%ss\') AS last_update_fr 
+            'SELECT id, title, header, content, DATE_FORMAT(last_update, \'%d/%m/%Y à %Hh%imin%ss\') AS last_update_fr 
             FROM post 
             ORDER BY last_update 
             DESC LIMIT 0, 5'
