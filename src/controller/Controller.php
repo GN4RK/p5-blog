c38d55d87bc3ea2ae4623f8dcc5c44375048f83a
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-05-31 21:19:42 +0200

    cleaning class Controller

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -2,22 +2,5 @@
 
 class Controller 
 {
-    public static function getNav() {
 
-        if (isset($_SESSION['user'])) {
-            return array(
-                array ('href' => BASEFOLDER, 'caption' => 'accueil'),
-                array ('href' => BASEFOLDER. "blog", 'caption' => 'blog'),
-                array ('href' => BASEFOLDER. "disconnection", 'caption' => "se déconnecter"),
-            );
-        } else {
-
-            return array(
-                array ('href' => BASEFOLDER, 'caption' => 'accueil'),
-                array ('href' => BASEFOLDER. "blog", 'caption' => 'blog'),
-                array ('href' => BASEFOLDER. "login", 'caption' => "se connecter / s'enregistrer"),
-            );
-        }
-
-    }
 }
