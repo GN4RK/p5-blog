8b99b65c4c416464c94c0f612c1bee21dfeecd0d
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-06-02 17:57:50 +0200

    added editPost function

--- src/controller/BackendController.php
+++ src/controller/BackendController.php
@@ -34,4 +34,27 @@ class BackendController extends Controller
     static function adminComment() {
         View::renderBack('comment.twig');
     }
+
+    static function editPost($id) {
+
+        $postManager = new PostManager();
+        $post = $postManager->getPost($id);
+
+        $postStatus = "";
+
+        if (isset($_POST['title']) && isset($_POST['content']) && isset($_POST['status'])) {
+            if (!empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['status'])) {
+                if (($_POST['title'] != $post['title']) || ($_POST['content'] != $post['content']) || ($_POST['status'] != $post['status'])) {
+
+                    $postManager->editPost($id, $_POST['title'], $_POST['content'], $_POST['status']);
+                    $postStatus = "post edited";
+                    $post = $postManager->getPost($id);
+
+                }
+            }
+        }
+        
+        View::renderBack('edit.twig', ["post" => $post, "postStatus" => $postStatus]);
+
+    }
 }
