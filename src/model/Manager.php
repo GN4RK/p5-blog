0de4c04db41bd713a85e6c6e9f880470ed255554
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-06-03 17:41:25 +0200

    added abstract prefix to the class

--- src/model/Manager.php
+++ src/model/Manager.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-class Manager
+abstract class Manager
 {
     protected function dbConnect(): PDO {
         $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
