d35714dd1a89b6efbadfc7048d493d630e64db11
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-06-10 02:30:26 +0200

    added PostSG class to access $_POST

---
+++ src/model/PostSG.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model;
+
+class PostSG {
+
+    public static function set($key, $value): void {
+        $_POST[$key] = $value;
+    }
+
+    public static function get($key) {
+        return (isset($_POST[$key]) ? $_POST[$key] : null);
+    }
+
+    public static function forget($key): void {
+        unset($_POST[$key]);
+    }
+
+}
